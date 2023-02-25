4b7c59124191db00922cd292ffc8a8da4992a7a7
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-02-24 21:18:25 -0500

    Implemnted sending data from inout to the server

--- play.js
+++ play.js
@@ -1,24 +1,11 @@
 const net = require("net");
 const connect = require("./client.js");
+const setupInput = require("./input.js");
 
 
 
 console.log("Connecting ...");
-connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = function () {  
-  process.stdin.on('data', (key) => {
-    if (key.ctrl && key.name == "C") {
-      process.exit();
-    }
-  });  
-};
+setupInput(connect());
+
